07b2319c7e1bd4e7daeec0a3262c75f9e2cf0951
Tobias Kraus <tobias.kraus@pemedia.de>
2022-01-26 20:47:52 +0100

    update package.json & remove a11y plugin

--- pem-kanban/.eslintrc.js
+++ pem-kanban/.eslintrc.js
@@ -5,14 +5,11 @@ module.exports = {
         // Order: eslint, typescript-eslint, plugins, prettier
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
-        'plugin:jsx-a11y/recommended',
         'prettier',
     ],
     env: {
-        // browser: true,
-        // es6: true,
-        // jest: true,
-        // node: true,
+        browser: true,
+        node: true,
     },
     rules: {
         // Order: eslint, typescript-eslint, plugins
