4dd4b6e4ab3d73d0133db8bc55218bd2fa47db1a
Tobias Kraus <tobias.kraus@pemedia.de>
2022-02-16 23:49:49 +0100

    derived state & createEffect

--- pem-kanban/src/App.tsx
+++ pem-kanban/src/App.tsx
@@ -1,18 +1,23 @@
-import { Component, createSignal } from 'solid-js';
+import { Component, createEffect, createSignal } from 'solid-js';
 
 import styles from './App.module.css';
 
 const App: Component = () => {
     const [count, setCount] = createSignal(0);
+    const doubleCount = () => count() * 2;
 
     const onClick = () => {
         setCount((_count) => _count + 1);
     };
 
+    createEffect(() => {
+        console.log('send current doubleCount to API:', doubleCount());
+    });
+
     return (
         <div class={styles.App}>
             <header class={styles.header}>
-                <h2>{count()}</h2>
+                <h2>{doubleCount()}</h2>
                 <button onClick={onClick}>click me</button>
             </header>
         </div>
