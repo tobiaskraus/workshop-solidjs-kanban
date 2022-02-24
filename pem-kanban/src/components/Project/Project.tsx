22832a4f90f8bc8b23ffbb46ffa6db71c0294318
Tobias Kraus <tobias.kraus@pemedia.de>
2022-02-24 22:44:27 +0100

    Project.tsx: complicated way which doesn't work...

--- pem-kanban/src/components/Project/Project.tsx
+++ pem-kanban/src/components/Project/Project.tsx
@@ -1,10 +1,47 @@
-import { Component } from 'solid-js';
-import { selectedProject } from '../../store';
+import { Component, For } from 'solid-js';
+import { Column } from '../../models/column';
+import { Ticket } from '../../models/ticket';
+import { selectedProject, tickets } from '../../store';
 
 import styles from './Project.module.css';
 
+type ProjectData = { [key in Column]: Ticket[] };
+
 const Project: Component = () => {
-    return <h1>Project view: {selectedProject}</h1>;
+    const ticketsOfProject = tickets().filter(
+        (ticket) => ticket.project.id === selectedProject()?.id
+    );
+    const columnsOfProject = selectedProject().columns;
+    const projectData = columnsOfProject.reduce(
+        (acc, val) => ({
+            ...acc,
+            [val]: ticketsOfProject.filter((ticket) => ticket.column === val),
+        }),
+        {} as ProjectData
+    );
+
+    return (
+        <>
+            <h1>Project view: {selectedProject()?.slug}</h1>
+            <div class={styles.columns}>
+                <For each={Object.entries(projectData)}>
+                    {([column, tickets]) => (
+                        <div class={styles.column}>
+                            <p>{column}</p>
+                            <Tickets tickets={tickets} />
+                        </div>
+                    )}
+                </For>
+            </div>
+        </>
+    );
 };
 
 export default Project;
+
+interface TicketsProps {
+    tickets: Ticket[];
+}
+const Tickets: Component<TicketsProps> = (props) => (
+    <For each={props.tickets}>{(ticket) => <div class={styles.ticket}>{ticket.title}</div>}</For>
+);
